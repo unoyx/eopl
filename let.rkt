c74e25d5477f80b037ad8790e3abc86fc184abef
heshan <icywindhe@qq.com>
2024-02-16 12:09:35 +0800

    finish practice 3.10

--- let.rkt
+++ let.rkt
@@ -24,11 +24,15 @@
         (begin (eopl:printf "~, " (car env))
                (show-env (cdr env))))))
 
+;; 表示expression的可能返回值类型
 (define-datatype expval expval?
   (num-val
    (num number?))
   (bool-val
-   (bool boolean?)))
+   (bool boolean?))
+  (list-val
+   (my-list (lambda (x)
+              (or pair? null?)))))
 
 (define (report-expval-extractor-error arg val)
   (eopl:error 'extracte-data "arg is: ~s, val is: ~s" arg val))
@@ -45,6 +49,12 @@
            (bool-val (bool) bool)
            (else (report-expval-extractor-error 'bool val)))))
 
+(define expval->list
+  (lambda (val)
+    (cases expval val
+           (list-val (my-list) my-list)
+           (else (report-expval-extractor-error 'list val)))))
+
 (define init-env
   (lambda ()
     empty-env))
@@ -54,11 +64,32 @@
     (cases program pgm
            (a-program (exp1)
                       (value-of exp1 env)))))
+
 (define value-of
   (lambda (exp env)
     (cases expression exp
+           (empty-list-exp () (list-val empty))
+           (null?-exp (exp) (bool-val (null? (expval->list (value-of exp env)))))
            (const-exp (num) (num-val num))
            (var-exp (var) (apply-env env var))
+           (minus-exp (exp) (num-val (- (expval->num (value-of exp env)))))
+           (car-exp (exp) (car (expval->list (value-of exp env))))
+           (cdr-exp (exp) (cdr (expval->list (value-of exp env))))
+           (create-list-exp (exp) (letrec [(exp-res (map (lambda (e)
+                                                           (value-of e env)) exp))
+                                           ;; 遍历一个列表，将每个元素包裹在list-val中
+                                           (list-val-iter (lambda (the-list)
+                                                            (if (null? the-list)
+                                                                (list-val empty)
+                                                                (list-val (cons (car the-list)
+                                                                                (list-val-iter (cdr the-list)))))))]
+                                    (list-val-iter exp-res)))
+           (cons-exp (exp1 exp2)
+                     (let ((val1 (value-of exp1 env))
+                           (val2 (value-of exp2 env)))
+                       ;; cons操作可以处理所有expval，所以这里没有取value的操作
+                       (list-val
+                        (cons val1 val2))))
            (diff-exp (exp1 exp2)
                      (let ((val1 (value-of exp1 env))
                            (val2 (value-of exp2 env)))
@@ -66,6 +97,20 @@
                              (num2 (expval->num val2)))
                          (num-val
                           (- num1 num2)))))
+           (add-exp (exp1 exp2)
+                    (let ((val1 (value-of exp1 env))
+                          (val2 (value-of exp2 env)))
+                      (let ((num1 (expval->num val1))
+                            (num2 (expval->num val2)))
+                        (num-val
+                         (+ num1 num2)))))
+           (equal?-exp (exp1 exp2)
+                       (let ((val1 (value-of exp1 env))
+                             (val2 (value-of exp2 env)))
+                         (let ((num1 (expval->num val1))
+                               (num2 (expval->num val2)))
+                           (bool-val
+                            (= num1 num2)))))
            (zero?-exp (exp1)
                       (let ((val1 (value-of exp1 env)))
                         (let ((num1 (expval->num val1)))
@@ -96,11 +141,38 @@
      (number)
      const-exp)
     (expression
+     ("list" "(" (separated-list expression ",") ")")
+     create-list-exp)
+    (expression
+     ("emptylist")
+     empty-list-exp)
+    (expression
+     ("null?" "(" expression ")")
+     null?-exp)
+    (expression
+     ("car" "(" expression ")")
+     car-exp)
+    (expression
+     ("cdr" "(" expression ")")
+     cdr-exp)
+    (expression
+     ("cons" "(" expression "," expression ")")
+     cons-exp)
+    (expression
+     ("minus" "(" expression ")")
+     minus-exp)
+    (expression
      ("-" "(" expression "," expression ")")
      diff-exp)
     (expression
+     ("+" "(" expression "," expression ")")
+     add-exp)
+    (expression
      ("zero?" "(" expression ")")
      zero?-exp)
+    (expression
+     ("equal?" "(" expression "," expression ")")
+     equal?-exp)
     (expression
      ("if" expression "then" expression "else" expression)
      if-exp)
@@ -124,20 +196,25 @@
 (define scan&parse
   (sllgen:make-string-parser scanner-let grammar-let))
 
+;; test cases
+
 (define test-driver
   (lambda (case)
     (letrec [(env empty-env)
              (res (value-of-program (scan&parse case) env))]
-      (eopl:printf "case: ~a, result: ~a, env: ~a\n" case res env))))
+      (eopl:printf "case: ~a, result: ~a\n" case res))))
 
 (test-driver "3")
-
 (test-driver "-(2, 3)")
-
 (test-driver "zero? (2)")
-
 (test-driver "zero? (0)")
-
 (test-driver "if zero? (0) then 1 else 2")
-
 (test-driver "let a = 10 in -(a, 9)")
+(test-driver "minus(-(minus(5),9))")
+(test-driver "equal?(+(2, minus(3)), minus(1))")
+(test-driver "null? (emptylist)")
+(test-driver "cons (2, emptylist)")
+(test-driver "+(car(cons(2, emptylist)), 1)")
+(test-driver "let x = 4 in cons(x,cons(cons(-(x,1),emptylist),emptylist))")
+(test-driver "list(1, 2, 3)")
+(test-driver "equal?(+(car(list(1, 2, 3)),4),5)")
